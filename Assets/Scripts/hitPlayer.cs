f9b3e6ebf6f53122456a399018c85be1bae929d2
OriHoward <ori.howard@gmail.com>
2023-04-06 15:57:06 +0300

    Added winking when a player is hit

--- Assets/Scripts/hitPlayer.cs
+++ Assets/Scripts/hitPlayer.cs
@@ -5,20 +5,48 @@ using UnityEngine;
 public class hitPlayer : MonoBehaviour
 {
     [SerializeField] string triggerTag;
-    private float ignoreTimer = 1.0f;
+    [SerializeField] float visibilityDuration = 2.0f;
+    [SerializeField] float winkSpeed = 10.0f;
+
+    private float visibilityTimer = 0.0f;
+
+    private bool isWinking = false;
+
+    private Renderer otherRenderer;
+    private float ignoreTimer;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (ignoreTimer <= 0 && other.CompareTag(triggerTag))
+        if (visibilityTimer <= 0.0f && ignoreTimer <= 0 && other.CompareTag(triggerTag))
         {
             Debug.Log("hit!");
+            otherRenderer = other.GetComponent<Renderer>();
             ignoreTimer = 1.0f;
+            visibilityTimer = visibilityDuration;
+            isWinking = true;
 
         }
+    }
+    private void Start()
+    {
+        ignoreTimer = 1.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
         ignoreTimer -= Time.deltaTime;
+        if (isWinking)
+        {
+            float visibilityAmount = Mathf.PingPong(Time.time * winkSpeed, 1);
+
+            otherRenderer.enabled = visibilityAmount > 0.5f;
+            if (visibilityTimer <= 0)
+            {
+                isWinking = false;
+                otherRenderer.enabled = true;
+            }
+            visibilityTimer -= Time.deltaTime;
+        }
+
     }
 }
